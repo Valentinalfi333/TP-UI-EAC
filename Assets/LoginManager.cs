93b120238668ad8a27ba87a38a8a5d3a1afb1a5d
ALFIE VALENTIN <48178972@ort>
2024-05-27 14:15:49 -0300

    TPUIFINISH

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LoginManager : MonoBehaviour
+{
+    public InputField contraseña;
+
+    public void MostrarEnConsola()
+    {
+        string contraseñareal = "miguelito";
+        if (contraseña.text == contraseñareal) {
+            Debug.Log("Acceso garantizado");
+        }
+        else
+        {
+            Debug.Log("Acceso denegado");
+        }
+    }
+}
